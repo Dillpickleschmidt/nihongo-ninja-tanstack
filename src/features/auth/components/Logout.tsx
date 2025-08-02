a4b6d4549ef5c4bceb9fa48090ed558cab8b7a45
Dillpickleschmidt <drkuzmick@gmail.com>
2025-08-02 19:22:16 -0400

    show logout component + clear session storage on logout

--- src/features/auth/components/Logout.tsx
+++ src/features/auth/components/Logout.tsx
@@ -1,8 +1,14 @@
 // features/auth/components/Logout.tsx
+import { clearFoldersAndDecks } from "@/features/vocab-page/storage/sessionStorage"
+
 export default function LogoutButton() {
+  const handleLogout = () => {
+    clearFoldersAndDecks()
+  }
+
   return (
-    <form action="/api/auth/logout" method="post">
-      <button type="submit" class="rounded bg-red-600 px-3 py-1 text-white">
+    <form action="/api/auth/logout" method="post" onSubmit={handleLogout}>
+      <button type="submit" class="rounded bg-red-500 px-5 py-2 text-white">
         Logout
       </button>
     </form>
--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,20 +1,44 @@
 // src/routes/auth.tsx
 import { createFileRoute } from "@tanstack/solid-router"
+import { Show } from "solid-js"
 import Login from "@/features/auth/components/Login"
+import LogoutButton from "@/features/auth/components/Logout"
 
 export const Route = createFileRoute("/auth")({
+  loader: ({ context }) => {
+    return {
+      user: context.user,
+    }
+  },
   component: AuthPage,
 })
 
 function AuthPage() {
+  const data = Route.useLoaderData()
+  const user = data().user
+
   return (
     <div class="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div class="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 class="mt-6 text-center text-2xl leading-9 font-bold tracking-tight text-gray-900 dark:text-white">
-          Sign in to your account
-        </h2>
+        <Show
+          when={user}
+          fallback={
+            <>
+              <h2 class="mt-6 text-center text-2xl leading-9 font-bold tracking-tight text-gray-900 dark:text-white">
+                Sign in to your account
+              </h2>
+              <Login callbackName="handleSignInWithGoogleCallback" />
+            </>
+          }
+        >
+          <h2 class="mt-6 text-center text-2xl leading-9 font-bold tracking-tight text-gray-900 dark:text-white">
+            You are signed in
+          </h2>
+          <div class="mt-6 flex justify-center">
+            <LogoutButton />
+          </div>
+        </Show>
       </div>
-      <Login callbackName="handleSignInWithGoogleCallback" />
     </div>
   )
 }
