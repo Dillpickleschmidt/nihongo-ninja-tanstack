9009464f6332ad91f3433d9a9cc36b57548c22d6
Dylan Kuzmick <drkuzmick@gmail.com>
2025-08-13 16:58:40 -0400

    add rollup-plugin-visualizer (for dev) to debug bundle size issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsConfigPaths from "vite-tsconfig-paths"
 import { tanstackStart } from "@tanstack/solid-start/plugin/vite"
 import viteSolid from "vite-plugin-solid"
 import lucidePreprocess from "vite-plugin-lucide-preprocess"
+import { visualizer } from "rollup-plugin-visualizer"
 
 export default defineConfig({
   server: {
@@ -17,5 +18,14 @@ export default defineConfig({
       customViteSolidPlugin: true,
     }),
     viteSolid({ ssr: true }),
+    process.env.ANALYZE
+      ? visualizer({
+          filename: "dist/bundle-report.html",
+          template: "treemap",
+          gzipSize: true,
+          brotliSize: true,
+          open: true,
+        })
+      : null,
   ],
 })
