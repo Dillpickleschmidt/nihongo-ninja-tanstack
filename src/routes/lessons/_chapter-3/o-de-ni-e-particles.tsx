6f6612bdd34ef6f492e1b2a7b435d003b79ba4ec
Dylan Kuzmick <drkuzmick@gmail.com>
2025-09-27 17:14:23 -0400

    force another redeploy

--- src/routes/lessons/_chapter-3/o-de-ni-e-particles.tsx
+++ src/routes/lessons/_chapter-3/o-de-ni-e-particles.tsx
@@ -1,3 +1,4 @@
+// routes/lessons/_chapter-3/o-de-ni-e-particles.tsx
 import { createFileRoute } from "@tanstack/solid-router"
 import Furigana from "@/components/text/Furigana"
 import YouTubeVideo from "@/features/youtube/YouTube"
