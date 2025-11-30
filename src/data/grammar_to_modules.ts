62e18eb004d122629d44106d5a0307a656414ddb
Dylan Kuzmick <drkuzmick@gmail.com>
2025-11-30 17:00:28 -0500

    update grammar pattern sync source

--- src/data/grammar_to_modules.ts
+++ src/data/grammar_to_modules.ts
@@ -5,26 +5,26 @@ import type { GrammarPatternId } from "./grammar_patterns"
  * Each grammar pattern can be taught in one or more modules
  */
 export const GRAMMAR_TO_MODULES: Partial<Record<GrammarPatternId, string[]>> = {
-  // "x_wa_y_desu": ["x-wa-y-desu", "sentence-practice-x-wa-y-desu", "wa-comparisons"],
-  // "ka_particle_ending": ["questions-with-ka", "sentence-practice-questions-with-ka"], // (only ending か - not 行くかどうかわからん)
-  // "no_particle_modifier": ["the-no-particle", "sentence-practice-the-no-particle"], // (not ending の - どこ行くの？)
+  "x_wa_y_desu": ["x-wa-y-desu", "sentence-practice-x-wa-y-desu", "wa-comparisons"],
+  "ka_particle_ending": ["questions-with-ka", "sentence-practice-questions-with-ka"], // (only ending か - not 行くかどうかわからん)
+  "no_particle_modifier": ["the-no-particle", "sentence-practice-the-no-particle"], // (not ending の - どこ行くの？)
   // "ga-particle_subject": ["ga-particle"],
   // "dare": ["dare", "sentence-practice-dare"],
   // "mo-also": ["mo-particle", "mo-particle1", "sentence-practice-mo-particle"],
   // "negative-noun": ["janai", "sentence-practice-janai"], // (also matches na-adjectives)
-  // "masu_form": ["verb-conj-masu", "conjugation-practice-masu"],
-  // "polite_negative: ["negative-masu-conj", "conjugation-practice-masu"],
+  "masu_form": ["verb-conj-masu", "conjugation-practice-masu"],
+  "polite_negative": ["negative-masu-conj", "conjugation-practice-masu"],
   masen_ka: ["polite-invitations", "sentence-practice-polite-invitations"],
   // "adverb": ["adverbs", "sentence-practice-frequency-adverbs", "adj-to-adv"],
   // iru_standalone ["iru-aru", "sentence-practice-iru-aru"], // (no preceeding て-form verb like 食べている)
   // aru_standalone ["iru-aru", "sentence-practice-iru-aru"], // (no preceeding で like である)
-  // polite_past: ["polite-past-tense", "conjugation-practice-polite-past-tense-verbs"],
+  polite_past: ["polite-past-tense", "conjugation-practice-polite-past-tense-verbs"],
   // to-and: ["to-particle", "sentence-practice-to-particle"], // (and/with, not things like という)
   // i-adjective: ["adjective-conjugation", "conjugation-practice-adjective"],
   // na-adjective: ["adjective-conjugation", "conjugation-practice-adjective"],
   // adj-noun-modification: ["adj-modifying-nouns", "sentence-practice-adjectives"],
-  // polite_volitional: ["polite-volitional"],
-  // te_form: ["te-form", "conjugation-practice-te-form", "te-form-connection"],
+  polite_volitional: ["polite-volitional"],
+  te_form: ["te-form", "conjugation-practice-te-form", "te-form-connection"],
   te_kudasai: ["te-kudasai"],
   te_mo_ii: ["te-mo-ii-desu"],
   te_wa_ikenai: ["te-wa-ikemasen"],
