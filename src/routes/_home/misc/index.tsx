1926fe5bcb1615ffcd424dd954c877878dc342e9
Dillpickleschmidt <drkuzmick@gmail.com>
2026-03-13 16:02:49 -0400

    add link to kanji-practice-sheet in misc/

--- src/routes/_home/misc/index.tsx
+++ src/routes/_home/misc/index.tsx
@@ -1,9 +1,23 @@
-import { createFileRoute } from "@tanstack/solid-router"
+import { createFileRoute, Link } from "@tanstack/solid-router"
 
 export const Route = createFileRoute("/_home/misc/")({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_home/misc/"!</div>
+  return (
+    <div class="mx-auto max-w-4xl space-y-4 p-6">
+      <h1 class="text-xl font-semibold">Misc</h1>
+      <ul class="space-y-2">
+        <li>
+          <Link
+            href="/misc/kanji-practice-sheet"
+            class="text-primary hover:underline"
+          >
+            Kanji Practice Sheet
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
 }
